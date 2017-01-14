0b8654a05edcb2728b3c784f80dc3e82a01727a1
PotatoSauceVFX <rj@potatosaucevfx.com>
2017-01-14 15:54:05 -0500

    Added various objects and functions. Also cleaned up methods.

--- src/org/usfirst/frc/team3770/robot/Robot.java
+++ src/org/usfirst/frc/team3770/robot/Robot.java
@@ -6,7 +6,11 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
+
+import org.usfirst.frc.team3770.robot.ActuatorDouble.ActuatorStatus;
+
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot
@@ -16,25 +20,38 @@ public class Robot extends IterativeRobot
     private final int RIGHT_STICK_USB_PORT   = 0;
     
     private final int LEFT_MOTOR_PORT    = 0;
-    private final int RIGHT_MOTOR_PORT   = 2;
+    private final int RIGHT_MOTOR_PORT   = 1;
+    private final int AUX_MOTOR_PORT	 = 2;
     
-    private final int SONAR_ANALOG_PORT   = 0;
+    private final int POT_ANALOG_PORT   = 0;
+    
+    private final int CYLINDER_IN_PORT = 1;
+    private final int CYLINDER_OUT_PORT = 0;
     
     // Declare objects
-    Talon leftMotor, rightMotor;
-    Joystick leftStick, rightStick;
-    AnalogInput sonar;
+    Talon leftMotor, rightMotor, auxMotor;
     
-    DrivePIDoutput driveOutput;
-    SonarPIDinput sonarInput;
+    Joystick leftStick, rightStick;
+    AnalogInput pot;
+    
+    // Dubug Utility Class
+    Debug debug;
     
     // Declare utility variables
-    double left,right;
+    double left,right, aux;
     
+    // Add the pneumatics compressor
+    Compressor compressor;
+    
+    // Add Pneumatic Cylinder
+    ActuatorDouble cylinder;
+    
+    /*
     PIDController approachControl;
     final double Kp = 0.2;
     final double Ki = 0.2;
     final double Kd = 0.2;
+    */
     
     //-------------------------------------------------
     public void robotInit() 
@@ -42,39 +59,74 @@ public class Robot extends IterativeRobot
         // Instantiate robot objects by calling constructors
         leftMotor  = new Talon(LEFT_MOTOR_PORT);      
         rightMotor = new Talon(RIGHT_MOTOR_PORT);
+        auxMotor = new Talon(AUX_MOTOR_PORT);
         
-       // leftMotor.setInverted(true);
-       // leftMotor.setInverted(true);
+        // Create Debug object
+        debug = new Debug();
         
+        // leftMotor.setInverted(true);
+        // leftMotor.setInverted(true);
+        
+        // Initialize the Joysticks
         leftStick  = new Joystick(LEFT_STICK_USB_PORT);     
         rightStick = new Joystick(RIGHT_STICK_USB_PORT);
         
-        sonar = new AnalogInput(SONAR_ANALOG_PORT);
         
+        pot = new AnalogInput(POT_ANALOG_PORT);
+        
+        // Initialize compressor and start it
+        compressor = new Compressor();
+        compressor.start();
+        
+        cylinder = new ActuatorDouble(CYLINDER_IN_PORT, CYLINDER_OUT_PORT, ActuatorStatus.IN);
+        
+        /*
         driveOutput = new DrivePIDoutput(leftMotor,rightMotor);
-        sonarInput = new SonarPIDinput(sonar);
+        pidSenseInput = new SonarPIDinput(pot);
         
-        approachControl = new PIDController( Kp,  Ki,  Kd, sonarInput, driveOutput);
+        approachControl = new PIDController( Kp,  Ki,  Kd, pidSenseInput, driveOutput);
         approachControl.setInputRange(0.0, 40.0);
         approachControl.setOutputRange(0, 1.0);     
-        approachControl.setSetpoint(15.0) ; 
+        
         approachControl.enable();
+        */
+        
+        // Clear the dashboard
+        debug.clearDashboard();
+        System.out.println("=============ROBOT INITIALIZED!=============");
     }
     
     //-------------------------------------------------
     public void teleopPeriodic() 
     {
+    	if(leftStick.getRawButton(3)) 
+    	{
+    		if(cylinder.getStatus() == ActuatorStatus.IN) 
+    		{
+    			cylinder.goOut();
+    		}
+    	}
+    	else if(leftStick.getRawButton(4)) 
+    	{
+    		if(cylinder.getStatus() == ActuatorStatus.OUT) 
+    		{
+    			cylinder.goIn();
+    		}
+    	}
+    	
         // Get joy stick values (-1 ... 0 ... 1)
         left  = leftStick.getY();
         right = rightStick.getY();
+        aux = (rightStick.getThrottle()+1)/2;
+        
         
         // Set drive motors to current joy stick values
         leftMotor.set(left);
         rightMotor.set(right);
+        auxMotor.set(aux);
         
+        //debug.print(0, "Pot: " + pidSenseInput.pidGet());
+        //debug.print(1, "PID: " + approachControl.get());
         
-        
-        SmartDashboard.putString("DB/String 0", "PID Source: " + sonarInput.pidGet());
-        SmartDashboard.putString("DB/String 1", "PID Output: " + approachControl.get());
     }
 }
