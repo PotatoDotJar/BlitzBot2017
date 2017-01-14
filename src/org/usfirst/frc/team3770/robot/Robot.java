74ee4660d94bfc2990c1e00c691d1bd49d39d34f
Blitz Creek Roboitcs <Blitz Creek Roboitcs@3770-PG>
2017-01-14 10:44:29 -0500

    Added sonar analog input and Pid controller functionality.

--- src/org/usfirst/frc/team3770/robot/Robot.java
+++ src/org/usfirst/frc/team3770/robot/Robot.java
@@ -1,44 +1,79 @@
+// Team 3770 Robot
+// PID testing
+
 package org.usfirst.frc.team3770.robot;
-
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- *
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SampleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're
- * inexperienced, don't. Unless you know what you are doing, complex code will
- * be much more difficult under this system. Use IterativeRobot or Command-Based
- * instead if you're new.
- */
-public class Robot extends SampleRobot {
-	RobotDrive myRobot = new RobotDrive(0, 1); // class that handles basic drive
-												// operations
-	Joystick leftStick = new Joystick(0); // set to ID 1 in DriverStation
-	Joystick rightStick = new Joystick(1); // set to ID 2 in DriverStation
-
-	public Robot() {
-		myRobot.setExpiration(0.1);
-	}
-
-	/**
-	 * Runs the motors with tank steering.
-	 */
-	@Override
-	public void operatorControl() {
-		myRobot.setSafetyEnabled(true);
-		while (isOperatorControl() && isEnabled()) {
-			myRobot.tankDrive(leftStick, rightStick);
-			Timer.delay(0.005); // wait for a motor update time
-		}
-	}
+public class Robot extends IterativeRobot
+{
+    // Declare constant values
+    private final int LEFT_STICK_USB_PORT    = 1;
+    private final int RIGHT_STICK_USB_PORT   = 0;
+    
+    private final int LEFT_MOTOR_PORT    = 0;
+    private final int RIGHT_MOTOR_PORT   = 2;
+    
+    private final int SONAR_ANALOG_PORT   = 0;
+    
+    // Declare objects
+    Talon leftMotor, rightMotor;
+    Joystick leftStick, rightStick;
+    AnalogInput sonar;
+    
+    DrivePIDoutput driveOutput;
+    SonarPIDinput sonarInput;
+    
+    // Declare utility variables
+    double left,right;
+    
+    PIDController approachControl;
+    final double Kp = 0.2;
+    final double Ki = 0.2;
+    final double Kd = 0.2;
+    
+    //-------------------------------------------------
+    public void robotInit() 
+    {
+        // Instantiate robot objects by calling constructors
+        leftMotor  = new Talon(LEFT_MOTOR_PORT);      
+        rightMotor = new Talon(RIGHT_MOTOR_PORT);
+        
+       // leftMotor.setInverted(true);
+       // leftMotor.setInverted(true);
+        
+        leftStick  = new Joystick(LEFT_STICK_USB_PORT);     
+        rightStick = new Joystick(RIGHT_STICK_USB_PORT);
+        
+        sonar = new AnalogInput(SONAR_ANALOG_PORT);
+        
+        driveOutput = new DrivePIDoutput(leftMotor,rightMotor);
+        sonarInput = new SonarPIDinput(sonar);
+        
+        approachControl = new PIDController( Kp,  Ki,  Kd, sonarInput, driveOutput);
+        approachControl.setInputRange(0.0, 40.0);
+        approachControl.setOutputRange(0, 1.0);     
+        approachControl.setSetpoint(15.0) ; 
+        approachControl.enable();
+    }
+    
+    //-------------------------------------------------
+    public void teleopPeriodic() 
+    {
+        // Get joy stick values (-1 ... 0 ... 1)
+        left  = leftStick.getY();
+        right = rightStick.getY();
+        
+        // Set drive motors to current joy stick values
+        leftMotor.set(left);
+        rightMotor.set(right);
+        
+        
+        SmartDashboard.putString("DB/String 0", "PID Source: " + sonarInput.pidGet());
+        SmartDashboard.putString("DB/String 1", "PID Output: " + approachControl.get());
+    }
 }
